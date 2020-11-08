7cacba184424ca0a4574e78746a427fc3f3a2440
Aileen Hara <34422734+aileenhara@users.noreply.github.com>
2020-11-08 18:09:41 -0500

    Update config.js
    adding key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: '<AAAAeEaPAnE:APA91bG9uW71K4aQqfQ6sAxsuMABP-hEcHjzOTg0S_dgJDsvFQX7-qhxhd53WI-HlH845E0hcFOX8WupE9oIDRhqvR-VGvXixk7wyhZ68MGtjVIKJEFCPBvmR4fj45t4L4tdt239-IyJ>'
 };
